1a204f7e6fb3dcb67ff9e81c4314856a97502076
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-25 11:34:35 -0300

    feat(context): add decorator to get current user

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { UserModule } from '@src/domain/user/user.module';
 import { AuthService } from '@src/auth/auth.service';
 import { LocalStrategy } from '@src/auth/strategies';
 import { JwtStrategy } from '@src/auth/jwt/jwt.strategy';
+import { LoginValidationMiddleware } from '@src/auth/middlewares/';
 
 @Module({
   imports: [UserModule, PassportModule, JwtConfigModule.register()],
