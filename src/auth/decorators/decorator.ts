1c29957ec8baae2c41665f13667de57ae41d3334
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-25 11:31:24 -0300

    feat(context): add decorator to get current user

--- src/auth/decorators/decorator.ts
+++ src/auth/decorators/decorator.ts
@@ -1,4 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '@src/domain/user/entities/user.entity';
+import { AuthRequest } from '@src/auth/models/auth.request.body';
 
 export enum KEY {
   IS_PUBLIC = 'isPublic',
@@ -6,3 +9,12 @@ export enum KEY {
 }
 export const IsPublic = () => SetMetadata(KEY.IS_PUBLIC, true);
 export const IsPrivate = () => SetMetadata(KEY.IS_PRIVATE, true);
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext): User => {
+    const { user } = context.switchToHttp().getRequest<AuthRequest>();
+    if (!user) return null;
+
+    return user;
+  },
+);
